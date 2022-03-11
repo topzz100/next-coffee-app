ef32460df28418b1fd76300ce314f77fdba52dfd
topzz100 <adenekantope@gmail.com>
2022-03-11 21:21:27 +0100

    Initial commit from Create Next App

---
+++ components/Navbar.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import styles from '../styles/Navbar.module.css'
+
+const Navbar = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.left}>
+        <div className={styles.callButton}>
+
+        </div>
+        <div className={styles.texts}>
+          <div className={styles.text}>
+
+          </div>
+          <div className={styles.text}>
+
+          </div>
+        </div>
+
+      </div>
+
+      <div className={styles.center}>
+
+      </div>
+
+      <div className={styles.right}>
+
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
